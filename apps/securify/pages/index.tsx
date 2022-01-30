093a5a1990413e579014a36ce76f6ea3c65601ac
geniusit <bertrand.deweer@gmail.com>
2022-01-30 01:04:56 +0100

    initial commit

---
+++ apps/securify/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export function Index() {
+
+  return (
+    <div className="text-blue-600">
+      Test
+    </div>
+  );
+}
+
+export default Index;
---
+++ apps/supa-securify/pages/index.tsx
@@ -0,0 +1,10 @@
+export function Index() {
+  return (
+
+    <div className="text-blue-600">
+      Test
+    </div>
+  );
+}
+
+export default Index;
